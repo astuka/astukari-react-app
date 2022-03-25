1acff66bb365f7da91f983ba3c997a3b9bd0a4a2
astuka <jrobins855@gmail.com>
2022-03-25 16:01:24 -0700

    Added rest of content/structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a basic app for testing create-react-app and the react feature set. 
+
+STATUS: Complete with potential for future iterations
--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-//import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* Vision for app: Links to sites + areas where you get latest updates, full list of projects */}
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to astukari. 
         </p>
-        <a className="App-link" href="https://jacob-robinson.com/">Blog</a>
-        <a className="App-link" href="https://astukari.substack.com/">Newsletter</a>
-        <a className="App-link" href="https://twitter.com/astukari">Twitter</a>
-        <a className="App-link" href="https://www.jbr-holdings.com/">JBR</a>
+        <div className="row">
+          <div className="column">
+            <h2>Projects</h2>
+            <a className="App-link" href="https://jacob-robinson.com/">Blog</a> <br/>
+            <a className="App-link" href="https://astukari.substack.com/">Newsletter</a> <br/>
+            <a className="App-link" href="https://www.apalla.co/">Apalla</a> <br/>
+            <a className="App-link" href="https://www.jbr-holdings.com/">JBR</a> <br/>
+            <a className="App-link" href="https://www.redbubble.com/people/astuka/">BDC LLC</a> <br/>
+          </div>
+          <div className="column">
+            <h2>Media</h2>
+            <a className="App-link" href="https://twitter.com/astukari">Twitter</a> <br/>
+            <a className="App-link" href="https://www.facebook.com/astukari">Facebook</a> <br/>
+            <a className="App-link" href="https://www.instagram.com/astukari/">Instagram</a> <br/>
+            <a className="App-link" href="https://www.youtube.com/channel/UCNFgvxw5igul8Xsymv2UhOg">Youtube</a> <br/>
+            <a className="App-link" href="https://www.pinterest.com/astukari/_saved/">Pinterest</a> 
+          </div>
+        </div>
       </header>
     </div>
   );
